fda6ebd5f8a246ae6f963442b96256048ae7eb98
Brandon Ly <brandon.ly@sjsu.edu>
2020-09-29 13:23:03 -0700

    2.1-2.4 randomizer

--- sketch.js
+++ sketch.js
@@ -1,8 +1,54 @@
+//favorite skate tricks
+let skate = [{
+  name: "kickflip",
+  difficulty: "3"
+}, {
+  name: "treflip",
+  diffuculty: "4"
+}, {
+  name: "Hardflip",
+  difficulty: "5"
+}, {
+  name: "heelflip",
+  difficulty: "3"
+}, {
+  name: "shuvit",
+  difficulty: "2"
+}, {
+  name: "ollie",
+  difficulty: "1"
+}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
+
+  //skate[1] ="treflip flip";
+  //console.log(skate);
+  //console.log("initial arrays is ");
+  //console.log(skate);
+
+  //skate.splice(4,1);
+  //console.log("array after splice")
+  //console.log(skate);
+
+  //skate.unshift("fakieflip");
+  //console.log("array after unshift")
+  //console.log(skate);
+
 }
 
 function draw() {
-  background(220);
-  square(60, 55, 55);
+  //square(60, 55, 55);
+}
+
+function mousePressed(){
+  background(random(200,250));
+  randomIndex = int(random(skate.length))
+  //console.log(skate[randomIndex].name);
+  text(skate[randomIndex].name, 100, 100);
+  skate.splice(randomIndex, 1);
 }
