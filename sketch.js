fee0a67b3fe0b0bb64a56ef7a963eda566f92fd4
Brandon Ly <brandon.ly@sjsu.edu>
2020-10-04 00:45:27 -0700

    2.2

--- sketch.js
+++ sketch.js
@@ -20,60 +20,42 @@ let skate = [{
 }];
 
 let randomIndex;
-let counter = 0;
+let animating = false;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
-  textSize(32);
+  textSize(22);
 
   text("click to randomize", 50, 50);
-
-  setTimeout(changeBackground, 1000);
-  //setInterval(changeBackground, 1000);
-
-
-  //skate[1] ="treflip flip";
-  //console.log(skate);
-  //console.log("initial arrays is ");
-  //console.log(skate);
-
-  //skate.splice(4,1);
-  //console.log("array after splice")
-  //console.log(skate);
-
-  //skate.unshift("fakieflip");
-  //console.log("array after unshift")
-  //console.log(skate);
 
 }
 
 function draw() {
-  //square(60, 55, 55);
+  if (animating == true) {
+    square(random(width), random(height), random(50, 200));
+  }
 }
 
-function changeBackground(){
- if (counter<= 5){
-   counter++;
-   console.log(counter);
- background(random(255), random(255), random(255));
- setTimeout(changeBackground, 1000);
-} else{
+function randomizer() {
+  animating = false;
+  if (skate[0]) {
+    background(random(200, 250));
+    randomIndex = int(random(skate.length));
+    //console.log(skate[randomIndex].name);
+    text(`${skate[randomIndex].name}'s difficulty level is
+    ${skate[randomIndex].difficulty}`, 50, 50);
+    // text(skate[randomIndex].name + "'s difficulty level is " + skate[randomIndex].difficulty, 50, 50);
+    skate.splice(randomIndex, 1);
+  } else {
+    background(random(200, 250));
+    text("nothing left!", 50, 50);
+  }
 
 }
-}
 
-function mousePressed(){
+function mousePressed() {
+ animating = true;
+  setTimeout(randomizer, 2000);
 
-  if (skate[0]){
-
-  background(random(200,250));
-  randomIndex = int(random(skate.length));
-  //console.log(skate[randomIndex].name);
-  text(skate[randomIndex].name, 100, 100);
-  skate.splice(randomIndex, 1);
-} else {
-  background(random(200,250));
-  text("nothing left!", 50,50);
-}
 }
